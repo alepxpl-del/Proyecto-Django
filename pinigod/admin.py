52876259307b53252199ee5e5d50e443cdde5056
alepxpl-del <ale.px.pl@gmail.com>
2026-09-08 19:30:37 -0300

    Punto alcanzado: Generacion de html en /pinturas con IA

--- pinigod/admin.py
+++ pinigod/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
-# Register your models here.
+from pinigod.models import Pintura
+#Metodo 1 para registrar:
+#admin.site.register(Pintura)
+#Metodo 2 para registrar
+@admin.register(Pintura)
+class PinturaAdmin(admin.ModelAdmin):
+    #Columna visible en el listado de registros.
+    list_display= ("nombre", "fecha_de_creacion")
+    #Campo que funciona como link para entral al detalle/registro.
+    list_display_links= ("nombre",)
+    #Habilita la barra de busqueda de registros.
+    search_fields= ("nombre", "autor")
+    #Agrega el panel lateral de registros.
+    list_filter= ("fecha_de_creacion",)
+    #Determina el orden de los datos registrados.
+    ordering= ("nombre", "autor", "fecha_de_creacion")
+    #Fecha visible pero no editable.
+    readonly_fields= ("fecha_de_creacion",)
--- pinigod/urls.py
+++ pinigod/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from pinigod.views import pinigod, listado_de_pinturas
 
 urlpatterns = [
-    path('', pinigod),
-    path('pinturas/', listado_de_pinturas)
+    path('', pinigod, name="home"),
+    path('pinturas/', listado_de_pinturas, name="listar_pinturas"),
 ]
